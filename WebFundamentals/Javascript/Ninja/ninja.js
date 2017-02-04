7ce9b57ff71c85347cd53a30dd81a1ed1a891b95
Arnold Son <hyomin.son@alum.utoronto.ca>
2017-02-03 16:27:03 -0800

    02/03/17 commit

---
+++ WebFundamentals/Javascript/Ninja/ninja.js
@@ -0,0 +1,6 @@
+$(".boxes").click(function(){
+    $(this).hide();
+});
+$("button").click(function(){
+    $(".boxes").show();
+});
--- WebFundamentals/Javascript/Users/Users.js
+++ WebFundamentals/Javascript/Users/Users.js
@@ -1,8 +1,8 @@
 $("button").click(function () {
     $("tbody").after("<tr><td id='1'></td><td id= '2'></td><td id= '3'></td><td id= '4'></td></tr>");
-    test();
+    table();
 });
-function test() {
+function table() {
         var col1 = $("#FirstName").val();
         var col2 = $("#LastName").val();
         var col3 = $("#Email").val();
@@ -11,5 +11,6 @@ function test() {
         $("#2").append(col2);
         $("#3").append(col3);
         $("#4").append(col4);
+        // $("#4").hide();
         $("td").removeAttr("id");
-}
+};
