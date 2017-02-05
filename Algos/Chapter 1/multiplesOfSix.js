e3e2fea1b0775734e9fb829e2a36c4a47d284840
Arnold Son <hyomin.son@alum.utoronto.ca>
2017-02-05 11:58:51 -0800

    added multiplesOfSix.js and printNeg52To1066.js'

---
+++ Algos/Chapter 1/multiplesOfSix.js
@@ -0,0 +1,9 @@
+function multiplesOfSix(){
+    var i = 6;
+    while(i < 60001){
+        console.log(i);
+        i+=6;
+    }
+}
+
+multiplesOfSix();
---
+++ Algos/Chapter 1/printNeg52To1066.js
@@ -0,0 +1,7 @@
+function printNeg52To1066(){
+    for(var i = -52; i < 1067; i++){
+        console.log(i);
+    }
+}
+
+printNeg52To1066();
